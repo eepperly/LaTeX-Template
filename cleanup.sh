0bff554bb3ff83e5b1540cb4b6ccf26174f122a5
Ethan Epperly <eepperly@caltech.edu>
2024-07-25 13:55:29 -0700

    Add SIAM template

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,21 @@
-rm -r auto
-rm *~
-rm *.aux
-rm *.bbl
-rm *.blg
-rm *.pdf
-rm *.log
-rm *.brf
-rm *.out
-rm *.synctex.gz
+rm -r siam/auto
+rm siam/*~
+rm siam/*.aux
+rm siam/*.bbl
+rm siam/*.blg
+rm siam/*.pdf
+rm siam/*.log
+rm siam/*.brf
+rm siam/*.out
+rm siam/*.synctex.gz
+
+rm -r general/auto
+rm general/*~
+rm general/*.aux
+rm general/*.bbl
+rm general/*.blg
+rm general/*.pdf
+rm general/*.log
+rm general/*.brf
+rm general/*.out
+rm general/*.synctex.gz
